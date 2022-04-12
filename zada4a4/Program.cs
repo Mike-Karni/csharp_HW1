2b53c5a08ed207ec4b389540dd1caf3e8f54783d
mike_karni <3923499@mail.ru>
2022-04-12 18:44:06 +0300

    zada4a4 done

---
+++ zada4a4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3: программу, которая на вход принимает число и выдаёт, является ли число чётным 
+Console.Write("Введите число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int OstDel = a%2;
+if (OstDel == 0)
+{
+    Console.WriteLine("Введённое число является чётным");
+}
+else
+{
+    Console.WriteLine("Введённое число является нечётным");
+}
